09cd74125f8dff91032768a6530f5329b4724567
Eliezer Lobaton <eliezerlobaton39@gmail.com>
2023-10-31 12:44:12 -0300

    heroku config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
      */
     public function register(): void
     {
-        //
+        if (env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url)
     {
-        //
+    if (env('REDIRECT_HTTPS')) {
+        $url->formatScheme('https://');
+    }
     }
 }
